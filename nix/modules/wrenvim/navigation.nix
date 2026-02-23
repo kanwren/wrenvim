2b22a4d6ec78f3414e4255d15349c917177b2983
Nicole Wren <nicole@wren.systems>
2026-02-22 23:14:10 -0800

    navigation: re-enable safe leap labels

--- nix/modules/wrenvim/navigation.nix
+++ nix/modules/wrenvim/navigation.nix
@@ -90,7 +90,6 @@
         enable = true;
         luaConfig.post = /* lua */ ''
           local leap = require('leap')
-          leap.opts.safe_labels = {}
           vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
           vim.keymap.set({ 'n' }, 'S', '<Plug>(leap-from-window)')
           vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
